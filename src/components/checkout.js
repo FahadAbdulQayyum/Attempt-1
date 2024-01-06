6fbf1222deb8a20cf10fba1935fccea8329f1abd
Fahad Abdul Qayyum <fahad.45843@iqra.edu.pk>
2024-01-06 23:44:01 +0500

    Object

--- src/components/checkout.js
+++ src/components/checkout.js
@@ -5,35 +5,30 @@ const Checkout = () => {
 
     const { products } = useContext(Cntxt);
 
-    // let quantityProducts = products.map(v => v.productName === v.productName)
-    let quantityProducts = products.map(v => v.imgUrl)
+    // let quantityProducts = products.map(v => v.imgUrl)
+    let quantityProducts = products.map(v => v._id)
 
     let quantifyProducts = products.map((v, i) => {
-        // let quantifyProducts = products.forEach((v, i) => {
         for (let a = 0; a <= quantityProducts.length; a++) {
-            if (v.imgUrl === quantityProducts[a]) {
-                // console.log('v.imgUrl|||', v.imgUrl, quantityProducts[a])
-                return { ...v, quantity: v.quantity++ }
-                // return console.log('v.imgUrl|||', v.imgUrl, quantityProducts[a])
+            // if (v.imgUrl === quantityProducts[a]) {
+            if (v._id === quantityProducts[a]) {
+                console.log('v._id === quantityProducts[a]', v._id, quantityProducts[a])
+                return { ...v, quantity: v.quantity += 1 }
+                // return { ...v, quantity: v.quantity++ }
             }
             return { ...v }
         }
     })
-    // let quantifyProducts = products.forEach((v, i) => console.log('v.imgUrl === quantityProducts[i]', v.imgUrl === quantityProducts[i]))
 
     const unique = quantifyProducts.filter(
         (obj, index) =>
             quantifyProducts.findIndex((item) => item._id === obj._id) === index
     );
 
-    // quantifyProducts = quantifyProducts.filter(v => v._id !== v._id)
-
-    // console.log('quantifyProducts', quantifyProducts)
-    console.log('unique', unique)
-
     return (
         <div>
-            {unique.map(v => <>{v.productName}</>)}
+            {/* {unique.map(v => <>{v.productName}</>)} */}
+            {unique.map(v => <div>{v.productName + ' - ' + v.quantity}</div>)}
         </div>
     )
 }
