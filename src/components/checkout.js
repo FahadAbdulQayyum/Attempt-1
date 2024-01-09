2003752c4250e8f3afb8f81aa7a103d4e3063aef
Fahad Abdul Qayyum <fahad.45843@iqra.edu.pk>
2024-01-09 22:18:22 +0500

    btn styled

--- src/components/checkout.js
+++ src/components/checkout.js
@@ -13,7 +13,8 @@ const Checkout = () => {
             {products.map(v => <div className='flex'>{v.productName + ' - ' + v.quantity + ' - ' + v.productPrice}<p className='ml-2 cursor-pointer border rounded-full p-1 px-[5px] bg-slate-100 hover:scale-105' onClick={() => dispatch(removeProduct(v))}>❌</p></div>)
             }
             <p className='font-bold'>Total: {products.reduce((a, b) => b.productPrice + a, 0)}</p>
-        </div >
+            <button className='bg-blue-400 text-white p-2 rounded-sm font-bold hover:bg-blue-500'>Checkout</button>
+        </div>
     )
 }
 
